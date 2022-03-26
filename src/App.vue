a278df582c0e32105c6492be6d7ed4be22af342f
VictorBuch <vbuch18@student.aau.dk>
2022-03-26 12:45:08 +0100

    Lets go?

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,28 @@
+<script lang="ts">
+export default {
+	data() {
+		return {
+			pageShown: 'uploadImages',
+			loadedClusterArray: [[]],
+		};
+	},
+	methods: {
+		imagesLoaded(e) {
+			this.pageShown = 'cullingPage';
+			this.loadedClusterArray = e.array;
+		},
+	},
+};
+</script>
+
 <template>
-  <router-view></router-view>
+	<UploadImagesPage
+		v-if="pageShown === 'uploadImages'"
+		@loaded="imagesLoaded"
+	/>
+
+	<CullingPage
+		v-if="pageShown === 'cullingPage'"
+		:cluster-array="loadedClusterArray"
+	/>
 </template>
--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -61,7 +61,6 @@ declare global {
   const useRoute: typeof import('vue-router')['useRoute']
   const useRouter: typeof import('vue-router')['useRouter']
   const useSlots: typeof import('vue')['useSlots']
-  const useStore: typeof import('@/store')['useStore']
   const watch: typeof import('vue')['watch']
   const watchEffect: typeof import('vue')['watchEffect']
 }
